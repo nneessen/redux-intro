3038f0ec636014db09cbfb567cd30bfbb19fdc39
Nick Neessen <nick.neessen@gmail.com>
2024-02-21 19:08:23 -0600

    initial commit

---
+++ src/store.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+const initialState = {
+  balance: 0,
+  loanAmount: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdrawal":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
