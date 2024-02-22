0ca8d780cc28cd7ae6f2e9fd11e800618de84d26
Nick Neessen <nick.neessen@gmail.com>
2024-02-22 14:40:45 -0600

    mv createdAt from reducer to prepare

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -7,22 +7,17 @@ const initialState = {
   createdAt: "",
 };
 
-const CUSTOMER_ACTION_TYPES = {
-  CREATE_CUSTOMER: "customer/createCustomer",
-  UPDATE_CUSTOMER_NAME: "customer/updateCustomer",
-};
-
 const customerSlice = createSlice({
   name: "customer",
   initialState,
   reducers: {
     createCustomer: {
-      prepare(fullName, nationalID, createdAt) {
+      prepare(fullName, nationalID) {
         return {
           payload: {
             fullName,
             nationalID,
-            createdAt,
+            createdAt: new Date().toISOString(),
           },
         };
       },
@@ -30,7 +25,7 @@ const customerSlice = createSlice({
       reducer(state, action) {
         state.fullName = action.payload.fullName;
         state.nationalID = action.payload.nationalID;
-        state.createdAt = new Date().toISOString();
+        state.createdAt = action.payload.createdAt;
       },
     },
     updateName(state, action) {
