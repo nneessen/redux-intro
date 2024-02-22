a4c7d3c84d0d9182ac3ec43d416c1612746519b5
Nick Neessen <nick.neessen@gmail.com>
2024-02-21 19:20:45 -0600

    using redux classic version for learning purposes

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loanAmount: 0,
@@ -13,7 +15,12 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -25,3 +32,19 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 100 });
+store.dispatch({ type: "account/withdrawal", payload: 100 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+
+console.log(store.getState());
